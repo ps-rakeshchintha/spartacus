c3438cbb2ba07da7caed8ec3fab340976ff5bda4
ps-rakeshchintha <rakesh.chintha@neudesic.com>
2019-10-24 19:22:39 +0530

    Updates

---
+++ precache-manifest.685c712a461c06c5b32b9bc7e1ffbef1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bef9981f5a7fc0c586fa231a94f3ab74",
+    "url": "/spartacus/index.html"
+  },
+  {
+    "revision": "4ee8b8a95c7f695114c7",
+    "url": "/spartacus/static/css/2.3c5bbc55.chunk.css"
+  },
+  {
+    "revision": "4e5a06c45f53577906dc",
+    "url": "/spartacus/static/css/main.c80f506b.chunk.css"
+  },
+  {
+    "revision": "4ee8b8a95c7f695114c7",
+    "url": "/spartacus/static/js/2.4b67d00e.chunk.js"
+  },
+  {
+    "revision": "4e5a06c45f53577906dc",
+    "url": "/spartacus/static/js/main.784ec632.chunk.js"
+  },
+  {
+    "revision": "4ebef85d9c639f966702",
+    "url": "/spartacus/static/js/runtime-main.5a0509ad.js"
+  }
+]);
